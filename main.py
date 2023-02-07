d9e892519ed4eeb6b377af6d19df3a8c92c3eed5
AVedineev <119727031+avedineev@users.noreply.github.com>
2023-02-07 19:07:24 +0300

    2 fun

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-from utils import
 import os
+
+from utils import get_student_by_pk
 
 path_student = os.path.join('data', 'students.json')
 path_professions = os.path.join('data', 'professions.json')
 
-input("")
+number_student = int(input("Введите номер студента: "))
+student = get_student_by_pk(number_student)
 
-
-
-
-
-
-
-
-
+print(f"Студент {student['full_name']}")
+print(f"Знает {' '.join(student['skills'])}")
 
 
