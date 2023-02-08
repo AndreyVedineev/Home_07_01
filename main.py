1429c8862497bf1e88dde1666811ce7fa7000e36
AVedineev <119727031+avedineev@users.noreply.github.com>
2023-02-08 18:29:40 +0300

    finish_01

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from utils import get_student_by_pk, get_profession_by_title, check_fitness
 
-# path_student = os.path.join('data', 'students.json')
-# path_professions = os.path.join('data', 'professions.json')
+if __name__ == '__main__':
+    number_student = int(input("Введите номер студента: ").capitalize().strip())
+    student = get_student_by_pk(number_student)
 
-number_student = int(input("Введите номер студента: ").capitalize().strip())
-student = get_student_by_pk(number_student)
+    print(f"Студент {student['full_name']}")
+    print(f"Знает {' '.join(student['skills'])}")
 
-print(f"Студент {student['full_name']}")
-print(f"Знает {' '.join(student['skills'])}")
+    professions_student = input(
+        f"Выберите специальность для оценки студента {student['full_name']}: ").capitalize().strip()
+    professions = get_profession_by_title(professions_student)
 
-professions_student = input(f"Выберите специальность для оценки студента {student['full_name']}: ").capitalize().strip()
-professions = get_profession_by_title(professions_student)
+    check = check_fitness(student['skills'], professions['skills'])
 
-check = check_fitness(student['skills'], professions['skills'])
-
-print(f"Пригодность: {check['fit_percent']}")
-print(f"{student['full_name']} знает {' '.join(check['has'])}")
-print(f"{student['full_name']} не знает {' '.join(check['lacks'])}")
+    print(f"Пригодность: {check['fit_percent']}")
+    print(f"{student['full_name']} знает {' '.join(check['has'])}")
+    print(f"{student['full_name']} не знает {' '.join(check['lacks'])}")
